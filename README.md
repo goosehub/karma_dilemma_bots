210eabc6b8765c84a95a057a71a22215d5ba2192
Goose <goosemixes@gmail.com>
2017-07-25 11:30:22 -0400

    Remove all, add selfless_bottom_feeder
    all.php had function conflicts, would need to use classes and stuff to avoid that.

--- README.md
+++ README.md
@@ -5,12 +5,14 @@
 
 <p>Make sure you add your user_id and api_key before using any bots.</p>
 
-<p>Call all.php to call all the bots together.</p>
-
 <h2>List of Bots</h2>
 <ul>
 	<li>
 		<a href="https://github.com/goosehub/karma_dilemma_bots/blob/master/selfish_bottom_feeder.php">Selfish Bottom Feeder</a>
 		<p>This simple bot will make a low bid on all game auctions. On games, it makes whichever choice has the bigger potential payoff.</p>
+	</li>
+	<li>
+		<a href="https://github.com/goosehub/karma_dilemma_bots/blob/master/selfless_bottom_feeder.php">Selfless Bottom Feeder</a>
+		<p>This simple bot will make a low bid on all game auctions. On games, it makes whichever choice has the bigger potential payoff for the other player.</p>
 	</li>
 </ul>
---
+++ selfless_bottom_feeder.php
@@ -0,0 +1,130 @@
+<?php
+/*
+Selfless Bottom Feeder
+This simple bot will make a low bid on all game auctions
+On games, it makes whichever choice has the bigger potential payoff for the other player
+*/
+
+/*
+Authentication
+*/ 
+
+define('USER_ID', 1);
+define('API_KEY', '');
+
+/*
+Actions
+*/ 
+
+bid_on_games();
+play_games();
+
+/*
+Functions
+*/ 
+
+function bid_on_games() {
+	// Make this bid on all games, adjust as needed
+	$bid = 80;
+
+	// Get games on auction
+	$path = 'games_on_auction';
+	$data = array();
+	$response = send_request($path, $data);
+	if ($response->error) {
+		die();
+	}
+
+	// Foreach game on auction
+	foreach ($response->games_on_auction as $game) {
+
+		// If game has bid by you, skip
+		if ($game->has_bid_by_you) {
+			continue;
+		}
+
+		// Make bid
+		$path = 'game/bid';
+		$data = array();
+		$data['game_id'] = (int) $game->id;
+		$data['amount'] = $bid;
+		$response = send_request($path, $data);
+	}
+}
+
+function play_games() {
+	// Get your turn games
+	$path = 'started_games';
+	$data = array();
+	$response = send_request($path, $data);
+	if (!$response) {
+		die();
+	}
+
+	// Foreach game on auction
+	foreach ($response->started_games as $game) {
+
+		// If choice has already been made, skip
+		if ($game->your_choice_made) {
+			continue;
+		}
+
+		// Find the choice that has the best potential payoff for the other player
+		$best_payoff_amount = 0;
+		$best_payoff_choice = 0;
+		foreach ($game->payoffs as $payoff) {
+			if ($game->your_player_type) {
+				if ($payoff->secondary_payoff > $best_payoff_amount) {
+					$best_payoff_amount = $payoff->secondary_payoff;
+					$best_payoff_choice = $payoff->secondary_choice;
+				}
+			}
+			else {
+				if ($payoff->primary_payoff > $best_payoff_amount) {
+					$best_payoff_amount = $payoff->primary_payoff;
+					$best_payoff_choice = $payoff->primary_choice;
+				}
+			}
+		}
+
+		// Play the game
+		$path = 'game/play';
+		$data = array();
+		$data['game_id'] = (int) $game->id;
+		$data['choice'] = $best_payoff_choice;
+		$response = send_request($path, $data);
+	}
+}
+
+function send_request($path, $data) {
+	// Info for debug
+	echo 'Calling ' . $path . ': ' . print_r($data, true) . '<br>' . PHP_EOL;
+
+	// Add authentication to post
+	$data['user_id'] = USER_ID;
+	$data['api_key'] = API_KEY;
+	$post = json_encode($data);
+
+	// Create URL
+	$base_url = 'https://karmadilemma.com/api/';
+	$url = $base_url . $path;
+
+	// Perform API Call with CURL
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+	$raw_response = curl_exec($ch);
+
+	// Get Response
+	$response = json_decode($raw_response);
+
+	// Catch error
+	if ($response->error) {
+		echo $response->error_code . ' - ' . $response->error_message . '<br>' . PHP_EOL;
+	}
+
+	// Return response
+	return $response;
+}
